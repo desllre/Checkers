d35c0d4ff5598c9bbbc1279d7a48bc3ae4f1f5cc
desire <kenmaks73@mail.ru>
2022-11-04 19:21:51 +0300

    Объявлены методы класса Board, для движения и анализа фигур: isHere, move, checkStep

--- engine/board.cpp
+++ engine/board.cpp
@@ -8,24 +8,24 @@ Board::Board(uint16_t size, bool isWhiteBoard): size(size), isWhiteBoard(isWhite
 
     if (isWhiteBoard){
         for (uint16_t i = 0; i < size ; ++i){
-            if (i == size / 2 || i == size / 2 + 1) continue;
-
-            for (size_t j = 0; j < size ; ++j) {
-                if (i < size / 2){
-                    board[i][j] = 'P';
-                } else{
-                    board[i][j] = 'p';
+            if (i == size/2 || i == size/2 + 1) {
+                for (size_t j = 0; j < size ; ++j)
+                    board[i][j] = '0';
+            } else {
+                for (size_t j = 0; j < size ; ++j) {
+                    if (i < size / 2){
+                        board[i][j] = 'P';
+                    } else{
+                        board[i][j] = 'p';
+                    }
                 }
             }
-
         }
 
     } else {
 
     }
 }
-
-
 
 Board::~Board(){
     for (int i = 0; i < size; ++i) {
@@ -34,10 +34,40 @@ Board::~Board(){
     delete[] board;
 }
 
+bool Board::isHere(uint16_t posX, uint16_t posY) {
+    if (board[posX][posY] != '0') return true;
+    return false;
+}
 
+/*std::vector<std::pair<uint16_t, uint16_t>> Board::possibles(uint16_t x, uint16_t y){
+    std::vector<std::pair<uint16_t, uint16_t>> possibles;
 
+    char figureType = board[x][y];
 
+    if (figureType == 'P' || figureType == 'p'){
 
+    } else{
+
+    }
+
+    return possibles;
+}*/
+
+std::pair<uint16_t, uint16_t> checkStep(uint16_t posX, uint16_t posY, char direct){
+    return std::pair<uint16_t, uint16_t>();
+}
+
+bool Board::move(uint16_t beginX, uint16_t beginY, uint16_t endX, uint16_t endY){
+
+    char figureType = board[beginX][beginY];
+    if (figureType == '0') return false;
+
+    return true;
+}
+
+std::vector<std::pair<uint16_t, uint16_t>> Board::possibles(uint16_t posX, uint16_t posY){
+    return std::vector<std::pair<uint16_t, uint16_t>>();
+}
 
 
 
--- engine/board.h
+++ engine/board.h
@@ -8,6 +8,17 @@
 
 // обозначение фигур: 'P' - чёрная пешка, 'p' - малая пешка, 'K' - чёрная дамка, 'k' - белая дамка
 
+#include "cstdlib"
+#include "string"
+#include "vector"
+
+#pragma once
+
+// Здесь -1 означает чёрную фигуру, а 1 - белую
+
+// обозначение фигур: 'P' - чёрная пешка, 'p' - малая пешка, 'K' - чёрная дамка,
+// 'k' - белая дамка, отсутствие фигуры - '0'
+
 class Board{
 private:
 
@@ -15,12 +26,17 @@ public:
     Board(uint16_t size, bool isWhiteBoard);
     ~Board();
 
+    bool isHere(uint16_t posX, uint16_t posY); // проверка на наличие фигуры в данной точке
 
+    bool move(uint16_t beginX, uint16_t beginY, uint16_t endX, uint16_t endY); // движение фигуры. Возвращает true, если фигура успешно перемещена
+
+    std::vector<std::pair<uint16_t, uint16_t>> possibles(uint16_t posX, uint16_t posY); // проверка на возмножность походить
 
 private:
 
+    std::pair<uint16_t, uint16_t> checkStep(uint16_t posX, uint16_t posY, char direct); //
 
-
+private:
     char** board;
     uint16_t size;
     bool isWhiteBoard; // хранит информацию о том, какой стороне принадлежит борд
